6fbb69e04fc89740949e46fe9cf9e02c6f2e70b6
kirilltm04 <92577092+kirilltm04@users.noreply.github.com>
2022-03-16 22:18:33 +0100

    initial commit

--- Lab_4/item.py
+++ Lab_4/item.py
@@ -1,20 +1,49 @@
 class Item:
-
-    def __init__(self, name):
+    """
+    Class with the item objects and methods to work with them.
+    """
+    def __init__(self, name: str, description=None):
+        """
+        Contains the following parameters:
+        :param name: str
+        :param description: str / bool
+        """
         self.name = name
-        self.description = None
-
-    def __str__(self):
-        return "The [" + self.name + "] is here - " + self.description
-
-    def set_name(self, item_name):
-        self.name = item_name
-
-    def get_name(self):
-        return self.name
-
-    def set_description(self, description):
         self.description = description
 
-    def get_description(self):
+    def __str__(self) -> str:
+        """
+        Prints the info about the Item class object.
+        :return: str
+        """
+        return "The [" + self.name + "] is here - " + self.description
+
+    def set_name(self, item_name: str) -> None:
+        """
+        Updates the name of the item.
+        :param item_name: str
+        :return: None
+        """
+        self.name = item_name
+
+    def set_description(self, description: str) -> None:
+        """
+        Updates the description of the item.
+        :param description: str
+        :return: None
+        """
+        self.description = description
+
+    def get_name(self) -> str:
+        """
+        Represents the name parameter.
+        :return: str
+        """
+        return self.name
+
+    def get_description(self) -> str:
+        """
+        Represents the description parameter.
+        :return: str
+        """
         return self.description
